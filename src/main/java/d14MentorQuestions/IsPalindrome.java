ef5082023bc9a3d1595ddf7c77d7257c112ffa35
Your Name <ahmet.dikbayir@gmail.com>
2024-03-05 00:19:04 +0300

    Plaindrome question was solved

---
+++ src/main/java/d14MentorQuestions/IsPalindrome.java
@@ -0,0 +1,25 @@
+package d14MentorQuestions;
+
+import java.util.Scanner;
+
+public class IsPalindrome {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a word for checking whether it is a palindrome or not: ");
+        String str = scan.next();
+        isPalindrome(str);
+
+    }
+
+    public static void isPalindrome(String str){
+        String reversed= "";
+        for(int  i = str.length()-1; i>=0; i--){
+            reversed += str.charAt(i);
+        }
+        if(str.equalsIgnoreCase(reversed)){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Nooo");
+        }
+    }
+}
